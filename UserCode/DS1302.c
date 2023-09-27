fc23c50ae9a040e709a837a1c998edf6975b580d
jimchn <jimchn@qq.com>
2023-09-27 14:00:57 +0800

    修改与增加文件

--- UserCode/DS1302.c
+++ UserCode/DS1302.c
@@ -1,11 +1,39 @@
 #include "STC89.h"
 #include "ds1302.h"
 
+//DS1302与单片机的连接
+sbit SCK = P2 ^ 0;	
+sbit SDA = P2 ^ 1;
+sbit RST = P2 ^ 2;
+
+#define RST_CLR	RST=0
+#define RST_SET	RST=1
+
+#define SDA_CLR	SDA=0
+#define SDA_SET	SDA=1
+
+#define SCK_CLR	SCK=0
+#define SCK_SET	SCK=1
+
+#define ds1302_sec_add			0x80
+#define ds1302_min_add			0x82
+#define ds1302_hr_add			0x84
+#define ds1302_date_add			0x86
+#define ds1302_month_add		0x88
+#define ds1302_day_add			0x8a
+#define ds1302_year_add			0x8c
+#define ds1302_control_add		0x8e
+#define ds1302_charger_add		0x90
+#define ds1302_clkburst_add		0xbe
+
+extern unsigned char time_buf1[8];
+extern unsigned char time_buf[8] ;
+
 unsigned char time_buf1[8] = {20, 10, 6, 5, 12, 55, 00, 6}; //空年月日时分秒周
 unsigned char time_buf[8] ;                         //空年月日时分秒周
-/*------------------------------------------------
-           向DS1302写入一字节数据
-------------------------------------------------*/
+
+
+//向DS1302写入一字节数据
 void Ds1302_Write_Byte(unsigned char addr, unsigned char d)
 {
 
@@ -18,11 +46,11 @@ void Ds1302_Write_Byte(unsigned char addr, unsigned char d)
     {
         if(addr & 0x01)
         {
-            IO_SET;
+            SDA_SET;
         }
         else
         {
-            IO_CLR;
+            SDA_CLR;
         }
         SCK_SET;
         SCK_CLR;
@@ -34,11 +62,11 @@ void Ds1302_Write_Byte(unsigned char addr, unsigned char d)
     {
         if(d & 0x01)
         {
-            IO_SET;
+            SDA_SET;
         }
         else
         {
-            IO_CLR;
+            SDA_CLR;
         }
         SCK_SET;
         SCK_CLR;
@@ -64,11 +92,11 @@ unsigned char Ds1302_Read_Byte(unsigned char addr)
 
         if(addr & 0x01)
         {
-            IO_SET;
+            SDA_SET;
         }
         else
         {
-            IO_CLR;
+            SDA_CLR;
         }
         SCK_SET;
         SCK_CLR;
@@ -79,7 +107,7 @@ unsigned char Ds1302_Read_Byte(unsigned char addr)
     for(i = 0; i < 8; i ++)
     {
         temp = temp >> 1;
-        if(IO_R)
+        if(SDA)
         {
             temp |= 0x80;
         }
