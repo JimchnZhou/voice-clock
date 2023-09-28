b985522c2d74fde34ddef5362b722cff91301bf8
jimchn <jimchn@qq.com>
2023-09-28 15:20:49 +0800

    完善 WT588F 相关代码

--- UserCode/WT588.c
+++ UserCode/WT588.c
@@ -3,49 +3,40 @@
 #include "delay.h"
 
 sbit Busy = P3 ^ 5;
-sbit One_Line_Data = P3 ^ 6;
-sbit Two_line_SCL = P3 ^ 7;
+sbit One_Wire_SDA = P3 ^ 6;
+sbit Two_Wire_SCL = P3 ^ 6;
+sbit Two_Wire_SDA = P3 ^ 7;
 
-#define Two_line_SCL One_Line_SDA	//一线控制时的数据 是 二线控制时的时钟
-
-//WT588F一线串口单字节控制程序
-void Line_1A_WT588F(unsigned char Byte_Value)
+//WT588F一线单字节控制程序
+void One_Wire_Send_Byte(unsigned char Byte_Value)
 {
     unsigned char S_DATA, i;
     S_DATA = Byte_Value;
-    One_Line_Data = 0;
-    Delay_ms(5); //延时 5ms
-    for(i = 0; i < 8; i++)
+    One_Wire_SDA = 0;	//拉低数据线
+    Delay_ms(5);		//延时5ms
+    for(i = 0; i < 8; i++)	//1Byte=8bit
     {
-        if((S_DATA & 0X01) == 1)
+        if((S_DATA & 0X01) == 1)	//发送1
         {
-            One_Line_Data = 1;
+            One_Wire_SDA = 1;
             Delay_us(600); //延时 600us
-            One_Line_Data = 0;
+            One_Wire_SDA = 0;
             Delay_us(200); //延时 200us
         }
-        else
+        else						//发送0
         {
-            One_Line_Data = 1;
+            One_Wire_SDA = 1;
             Delay_us(200); //延时 200us
-            One_Line_Data = 0;
+            One_Wire_SDA = 0;
             Delay_us(600); //延时 600us
         }
         S_DATA = S_DATA >> 1;
     }
-    One_Line_Data = 1;
+    One_Wire_SDA = 1;				//发送完毕，拉高数据线
 }
 
-//一线串口双字节控制程序
-/*--------------------------------------
-;模块名称:Line_1A_WT588F_Couple(UI16 USER_DATA)
-;功 能:实现一线串口通信函数
-;入 参:USER_DATA
-;出 参:
-;one_line_DATA 控制 IO
-;-------------------------------------*/
-
-void Line_1A_WT588F_Couple(unsigned int Double_Value)
+//WT588F一线双字节控制程序
+void One_Wire_Send_Double(unsigned int Double_Value)
 {
     unsigned char i, num_temp;
     unsigned char B_DATA;
@@ -56,8 +47,9 @@ void Line_1A_WT588F_Couple(unsigned int Double_Value)
     ddata_temp >>= 8; //高字节移位到低字节
     ddata_temp |= pdata_temp; //移位后重新合并
     num_temp = 16;
+	
     One_Line_Data = 0; //拉低数据线
-    Delay_ms(5); //延时 5MS
+    Delay_ms(5); //延时 5mS
     B_DATA = ddata_temp & 0X0001;
     for(i = 0; i < num_temp; i++)
     {
@@ -89,135 +81,114 @@ void Line_1A_WT588F_Couple(unsigned int Double_Value)
     One_Line_Data = 1;
 }
 
-//一线串口单字节连码示例(F3 + 01 + F3 + 02)
+//WT588F一线单字节连码示例(F3 + 01 + F3 + 02)
 Void List_1A_Play_WT588F(void)
 {
-    Line_1A_WT588F(0xF3);
+    One_Wire_Send_Byte(0xF3);
     Delay_ms(2); //延时 2ms
-    Line_1A_WT588F(0x01);
+    One_Wire_Send_Byte(0x01);
     Delay_ms(5); //延时 5ms
-    Line_1A_WT588F(0xF3);
+    One_Wire_Send_Byte(0xF3);
     Delay_ms(2); //延时 2ms
-    Line_1A_WT588F(0x02);
+    One_Wire_Send_Byte(0x02);
     Delay_ms(5); //延时 5ms
 }
 
-//一线串口双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
+//WT588F一线双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
 Void List_1A_Play_WT588F_Couple(void)
 {
-    Line_1A_WT588F_Couple(0xFFF3);
+    One_Wire_Send_Double(0xFFF3);
     Delay_ms(5); //延时 5ms
-    Line_1A_WT588F_Couple(0x0001);
+    One_Wire_Send_Double(0x0001);
     Delay_ms(10); //延时 10ms
-    Line_1A_WT588F_Couple(0xFFF3);
+    One_Wire_Send_Double(0xFFF3);
     Delay_ms(5); //延时 5ms
-    Line_1A_WT588F_Couple(0x0001);
+    One_Wire_Send_Double(0x0001);
     Delay_ms(10); //延时 10ms
 }
 
-//二线串口单字节控制程序
-/*--------------------------------------
-;模块名称:Line_2A_WT588F(UC8 DDATA)
-;功 能:实现二线串口通信函数
-;入 参:DDATA 为发送数据
-;出 参:
-;CLK_2A 时钟线
-;P_DATA_2A 数据线
-;-------------------------------------*/
-
-#define CLK_2A P01
-#define P_DATA_2A P00
-void Line_2A_WT588F(unsigned char DDATA)
+//WT588F二线单字节控制程序
+void Two_Wire_Send_Byte(unsigned char Byte_Value)
 {
-    unsigned char S_DATA, j;
-    unsigned char B_DATA;
-    CLK_2A = 1; //拉高 CLK
-    P_DATA_2A = 1; //拉高 DATA
-    S_DATA = DDATA;
-    CLK_2A = 0; //拉低 CLK
-    Delay_ms(5); //延时 5ms
-    B_DATA = S_DATA & 0X01;
-    for(j = 0; j < 8; j++)
+    unsigned char S_DATA, i;
+    Two_Wire_SCL = 1;	//拉高 CLK
+    Two_Wire_SDA = 1;	//拉高 DATA
+    S_DATA = Byte_Value;
+	
+    Two_Wire_SCL = 0;	//拉低 CLK
+    Delay_ms(5);		//延时 5ms
+	
+    for(i = 0; i < 8; i++)
     {
-        CLK_2A = 0; //拉低 CLK
-        P_DATA_2A = B_DATA; //传输数据一位
-        Delay_us(300); //延时 300us
-        CLK_2A = 1; //拉高 CLK
-        Delay_us(300); //延时 300us
-        S_DATA = S_DATA >> 1;
-        B_DATA = S_DATA & 0X01;
+        Two_Wire_SCL = 0;				//拉低CLK，为发送数据做准备
+        Two_Wire_SDA = (S_DATA & 0X01); //传输低位数据
+        Delay_us(350);					//延时350us
+        Two_Wire_SCL = 1;				//拉高CLK,上升沿接收数据
+        Delay_us(350);					//延时350us
+        S_DATA = S_DATA >> 1;			//数据右移
     }
-    P_DATA_2A = 1;
-    CLK_2A = 1;
+    Two_Wire_SDA = 1;
+    Two_Wire_SCL = 1;
 }
 
-//二线串口双字节控制程序
-/*--------------------------------------
-;模块名称:Line_2A_WT588F(UI16 USER_DATA)
-;功 能:实现二线串口通信函数
-;入 参:USER_DATA 为发送数据
-;出 参:
-;CLK_2A 时钟线
-;DATA_2A 数据线
-;-------------------------------------*/
-
-#define CLK_2A P01
-#define DATA_2A P02
-void Line_2A_WT588F_Couple(unsigned int USER_DATA)
+//WT588F二线双字节控制程序
+void Two_Wire_Send_Double(unsigned int Double_Value)
 {
-    unsigned char i, num_temp;
-    unsigned int ddata_temp, pdata_temp;
-    ddata_temp = USER_DATA;
-    pdata_temp = ddata_temp & 0X00FF;
-    ddata_temp >>= 8;
-    pdata_temp <<= 8;
-    ddata_temp |= pdata_temp;
-    num_temp = 16;
-    CLK_2A = 0; //拉低 CLK
-    Delay_10us(500); //延时 5ms
-    for(j = 0; j < num_temp; j++)
+    unsigned char i;
+    unsigned int ddata_temp, pdata_temp;	
+    ddata_temp = Double_Value;
+
+	//高低字节交换
+    pdata_temp = ddata_temp & 0X00FF;	//取低字节
+    ddata_temp >>= 8;					//右移8位，高字节变为低字节
+    pdata_temp <<= 8;					//低字节左移，放到高字节
+    ddata_temp |= pdata_temp;			//合并
+	
+    Two_Wire_SCL = 0;	//拉低 CLK
+    Delay_ms(5);		//延时 5ms
+    for(i = 0; i < 16; i++)
     {
-        if(j == 8)
+        if(i == 8)
         {
-            CLK_2A = 1;
-            DATA_2A = 1;
-            Delay_N10us(200); //延时 2ms
-            CLK_2A = 0;
-            Delay_N10us(500); //延时 5ms
+            Two_Wire_SCL = 1;
+            Two_Wire_SDA = 1;
+            Delay_ms(2); //延时 2ms
+            Two_Wire_SCL = 0;
+            Delay_ms(5); //延时 5ms
         }
-        CLK_2A = 0;
-        DATA_2A = ddata_temp & 0X0001;
-        Delay_N10us(20); //延时 200us
-        CLK_2A = 1;
-        Delay_N10us(20); //延时 200us
+        Two_Wire_SCL = 0;
+        Two_Wire_SDA = ddata_temp & 0X0001;
+        Delay_us(200); //延时 200us
+        Two_Wire_SCL = 1;
+        Delay_us(200); //延时 200us
         ddata_temp = ddata_temp >> 1;
     }
-    CLK_2A = 1;
-    DATA_2A = 1;
+    Two_Wire_SCL = 1;
+    Two_Wire_SDA = 1;
 }
 
-//二线串口单字节连码示例(F3 + 01 + F3 + 02)
+//WT588F二线单字节连码示例(F3 + 01 + F3 + 02)
 void List_2A_Play_WT588F(void)
 {
-    Line_2A_WT588F(0xF3);
-    Delay_N10us(200); //延时 2ms
-    Line_2A_WT588F(0x01);
-    Delay_N10us(500); //延时 5ms
-    Line_2A_WT588F(0xF3);
-    Delay_N10us(200); //延时 2ms
-    Line_2A_WT588F(0x02);
-    Delay_N10us(500); //延时 5ms
+    Two_Wire_Send_Byte(0xF3);
+    Delay_ms(2); //延时 2ms
+    Two_Wire_Send_Byte(0x01);
+    Delay_ms(5); //延时 5ms
+    Two_Wire_Send_Byte(0xF3);
+    Delay_ms(2); //延时 2ms
+    Two_Wire_Send_Byte(0x02);
+    Delay_ms(5); //延时 5ms
 }
 
-//二线串口双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
+//WT588F二线双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
 void List_2A_Play_WT588F_Couple(void)
 {
-    Line_2A_WT588F_Couple(0xFFF3);
+    Two_Wire_Send_Double(0xFFF3);
     Delay_ms(5); //延时 5ms
-    Line_2A_WT588F_Couple(0x0001);
+    Two_Wire_Send_Double(0x0001);
     Delay_ms(10); //延时 10ms
-    Line_2A_WT588F_Couple(0xFFF3);
+    Two_Wire_Send_Double(0xFFF3);
     Delay_ms(5); //延时 5ms
-    Line_2A_WT588F_Couple(0x0002);
+    Two_Wire_Send_Double(0x0002);
     Delay_ms(10); //延时 10ms
 }
--- UserCode/WT588.h
+++ UserCode/WT588.h
@@ -1,6 +1,50 @@
-//零、一、二、三、四、五、六、七、八、九、十、百、千、万、亿
-//甲、乙、丙、丁、戊、己、庚、辛、壬、癸
-//子、丑、寅、卯、辰、巳、午、未、申、酉、戌、亥
-//年、月、日、时、分、秒、农历、闰、初
-//时间温度湿度
-//
+/*
+基本数字：
+	零、一、二、三、四、五、六、七、八、九、十/时、百、千、万、亿
+	甲、乙、丙、丁、戊、己、庚、辛、壬、癸
+	子、丑、寅、卯、辰、巳、午、未、申、酉、戌、亥
+
+辅助音：
+	欢迎使用、智能、语音、电子、时钟、门锁
+	系统、网络、正在、初始化、连接、请稍后
+	成功、失败、错误、请再试一次
+	
+功能转换：
+	时间、温度、湿度、设置、模式
+
+时间：
+	现在是、北京时间
+	年、月、日、天、点、分、秒
+	凌晨、早上、上午、中午、下午、傍晚、晚上、深夜
+	整点、半点、报时、闹钟
+	农历、闰、初
+	
+门锁：
+	请输入、管理员、指纹、密码、请刷卡、验证、请开门、电量过低
+	
+计算器：
+	加、减、乘、除、等于、归零
+*/
+
+//WT588F一线单字节控制程序
+void One_Wire_Send_Byte(unsigned char Byte_Value);
+
+//WT588F一线双字节控制程序
+void One_Wire_Send_Double(unsigned int Double_Value);
+
+//WT588F一线单字节连码示例(F3 + 01 + F3 + 02)
+Void List_1A_Play_WT588F(void);
+
+//WT588F一线双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
+Void List_1A_Play_WT588F_Couple(void);
+
+//WT588F二线单字节控制程序
+void Two_Wire_Send_Byte(unsigned char Byte_Value);
+//WT588F二线双字节控制程序
+void Two_Wire_Send_Double(unsigned int Double_Value);
+
+//WT588F二线单字节连码示例(F3 + 01 + F3 + 02)
+void List_2A_Play_WT588F(void);
+
+//WT588F二线双字节连码示例(FFF3 + 0001 + FFF3 + 0002)
+void List_2A_Play_WT588F_Couple(void);
